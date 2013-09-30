385f494380c04aace7942a261cc656fd5f073c61
Dennis Tel <dennis@MacBook-Pro-van-Dennis.local>
2013-09-30 12:15:06 +0200

    fix for individual movie reviews

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,34 +16,34 @@ class MoviesController < ApplicationController
 
   # POST /movies
   # POST /movies.json
-  def create
-    @movie = Movie.new(params[:movie])
+  # def create
+  #   @movie = Movie.new(params[:movie])
 
-    if @movie.save
-      render json: @movie, status: :created, location: @movie
-    else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @movie.save
+  #     render json: @movie, status: :created, location: @movie
+  #   else
+  #     render json: @movie.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # PATCH/PUT /movies/1
-  # PATCH/PUT /movies/1.json
-  def update
-    @movie = Movie.find(params[:id])
+  # # PATCH/PUT /movies/1
+  # # PATCH/PUT /movies/1.json
+  # def update
+  #   @movie = Movie.find(params[:id])
 
-    if @movie.update(params[:movie])
-      head :no_content
-    else
-      render json: @movie.errors, status: :unprocessable_entity
-    end
-  end
+  #   if @movie.update(params[:movie])
+  #     head :no_content
+  #   else
+  #     render json: @movie.errors, status: :unprocessable_entity
+  #   end
+  # end
 
-  # DELETE /movies/1
-  # DELETE /movies/1.json
-  def destroy
-    @movie = Movie.find(params[:id])
-    @movie.destroy
+  # # DELETE /movies/1
+  # # DELETE /movies/1.json
+  # def destroy
+  #   @movie = Movie.find(params[:id])
+  #   @movie.destroy
 
-    head :no_content
-  end
+  #   head :no_content
+  # end
 end
